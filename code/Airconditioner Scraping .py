371950f919af228cc4bed4ee408f2185b64313de
Athira G <99167684+Athirag2000@users.noreply.github.com>
2023-05-19 14:45:11 +0530

    These are the code used for scraping the required data.
    Signed-off-by: Athira G <99167684+Athirag2000@users.noreply.github.com>

---
+++ code/Airconditioner Scraping .py
@@ -0,0 +1,373 @@
+#importing required libraries
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+#Function to extract content from page
+
+def url_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    return soup
+
+#Function for product url
+
+def get_product_url(soup):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    product_link= []
+    start_link = "https://www.flipkart.com"
+    for item in soup.find_all('a',href=True, attrs={'class':'_1fQZEK'}):
+        rest_link = item['href']
+        product_link.append(start_link+rest_link)
+    return product_link
+
+
+#Function to extract names of products 
+
+def get_product_name(soup):
+    product_name= []
+    try:
+        name=soup.find('span',attrs={'class':"B_NuCI"}).text
+        product_name.append(name)
+    except:
+        name = "No product name available"
+        product_name.append(name)
+    return product_name
+
+
+#Function to extract price of products 
+
+def get_product_price(soup):
+    product_price = []
+    try:
+        price = soup.find('div',attrs={'class':"_30jeq3 _16Jk6d"}).text
+        product_price.append(price)
+    except:
+        price = "NO price available"
+        product_price.append(price)
+    return product_price
+
+
+#Function to extract ratings of products 
+
+def get_product_ratings(soup):
+    product_ratings = []
+    try:
+        ratings = soup.find('div', attrs = {'class':"_3LWZlK"}).text
+        product_ratings.append(ratings)
+    except:
+        ratings = "No rating available"
+        product_ratings.append(ratings)
+    return product_ratings
+
+
+#Function to extract capacity_in_tons of products 
+
+def get_capacity_in_tons(soup):
+    capacity_in_tons = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "capacity in tons":
+                capacity_in_tons = cells[1].text.strip()
+                break
+        if not capacity_in_tons:
+            capacity_in_tons = "No value available"
+    except:
+         capacity_in_tons = "No value available"
+    return capacity_in_tons
+
+
+#Function to extract model_name of products 
+
+def get_model_name(soup):
+    model_name = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "model name":
+                model_name = cells[1].text.strip()
+                break
+        if not model_name:
+            model_name = "No value available"
+    except:
+         model_name = "No value available"
+    return model_name
+
+
+#Function to extract product_type of products 
+
+def get_product_type(soup):
+    product_type = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "type":
+                product_type = cells[1].text.strip()
+                break
+        if not product_type:
+            product_type = "No value available"
+    except:
+        product_type = "No value available"
+    return product_type
+
+
+#Function to extract brand of products 
+
+def get_brand(soup):
+    brand = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "brand":
+                brand = cells[1].text.strip()
+                break
+        if not brand:
+            brand = "No value available"
+    except:
+        brand = "No value available"
+    return brand
+
+
+#Function to extract product_color  of products 
+
+def get_product_color(soup):
+    color = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "color":
+                color = cells[1].text.strip()
+                break
+        if not color:
+            color = "No value available"
+    except:
+        color = "No value available"
+    return color
+
+
+#Function to extract cooling_capacity of products 
+
+def get_cooling_capacity(soup):
+    cooling_capacity = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "cooling capacity":
+                cooling_capacity = cells[1].text.strip()
+                break
+        if not cooling_capacity:
+            cooling_capacity = "No value available"
+    except:
+        cooling_capacity = "No value available"
+    return cooling_capacity
+
+
+#Function to extract compressor of products 
+
+def get_compressor(soup):
+    compressor = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "compressor":
+                compressor = cells[1].text.strip()
+                break
+        if not compressor:
+            compressor = "No value available"
+    except:
+        compressor = "No value available"
+    return compressor
+
+
+#Function to extract remote_control availability of products 
+
+def get_remote_control(soup):
+    remote_control = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "remote control":
+                remote_control = cells[1].text.strip()
+                break
+        if not remote_control:
+            remote_control = "No value available"
+    except:
+        remote_control = "No value available"
+    return remote_control
+
+
+
+#Function to extract dust_filter of products 
+
+def get_dust_filter(soup):
+    dust_filter = ""
+    try:
+        table = soup.find_all("div", {"class": "_3k-BhJ"})[4]
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "dust filter":
+                dust_filter = cells[1].text.strip()
+                break
+        if not dust_filter:
+            dust_filter = "No value available"
+    except:
+        dust_filter = "No value available"
+    return dust_filter
+
+
+
+#Function to extract power_requirement of products 
+
+def get_power_requirement(soup):
+    power_requirement = ""
+    try:
+        table = soup.find_all("div", {"class": "_3k-BhJ"})[6]
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "power requirement":
+                power_requirement = cells[1].text.strip()
+                break
+        if not power_requirement:
+             power_requirement = "No value available"
+    except:
+         power_requirement = "No value available"
+    return power_requirement
+
+
+#Function to extract power_consumption of products 
+
+def get_power_consumption(soup):
+    power_consumption = ""
+    try:
+        table = soup.find_all("div", {"class": "_3k-BhJ"})[6]
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "power consumption":
+                power_consumption = cells[1].text.strip()
+                continue
+        if not power_consumption:
+            power_consumption = "No value available"
+    except:
+        power_consumption = "No value available"
+    return power_consumption
+
+
+# Define the URL
+url_base = "https://www.flipkart.com/search?q=air%20conditioner&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+
+# Create an empty list to store data frames
+df_list = []
+
+# Scraping 
+for page in range(1,45):
+    # Define the URL for each page
+    url = url_base + str(page)
+    
+    # Get the content of the page
+    url_contents = url_content(url)
+    
+    # Get the URLs for each product on the page
+    product_links = get_product_url(url_contents)
+    
+    # Create an empty data frame
+    data = pd.DataFrame(columns=[
+        
+        
+        "ProductName",
+        "Price",
+        "Ratings",
+        "Capacity in tons",
+        "ModelName",
+        "ProductType",
+        "Brand",
+        "ProductColor",
+        "CoolingCapacity",
+        "Compressor",
+        "RemoteControl",
+        "DustFilter",
+        "PowerRequirement",
+        "PowerConsumption",
+        "Product_Url"
+       
+    ])
+
+    # Loop through each product on the page
+    for product in range(len(product_links)):
+        # Get the content of the product page
+        product_url = product_links[product]
+        product_content = url_content(product_url)
+
+        # Get the product details and store them in a data frame
+        product_names = get_product_name(product_content)
+        product_prices = get_product_price(product_content)
+        product_ratings= get_product_ratings(product_content)
+        capacity_in_tons=get_capacity_in_tons(product_content)
+        model_name = get_model_name(product_content)
+        product_type = get_product_type(product_content)
+        brand = get_brand(product_content)
+        product_color= get_product_color(product_content)
+        cooling_capacity= get_cooling_capacity(product_content)
+        compressor= get_compressor(product_content)
+        remote_control= get_remote_control(product_content)
+        dust_filter= get_dust_filter(product_content)
+        power_requirement= get_power_requirement(product_content)
+        power_consumption= get_power_consumption(product_content)
+      
+        df = pd.DataFrame({
+            "ProductName": product_names,
+            "Price": product_prices,
+            "Ratings" : product_ratings,
+            "Capacity in tons":capacity_in_tons,
+            "ModelName":model_name,
+            "ProductType":product_type,
+            "Brand":brand,
+            "ProductColor":product_color,
+            "CoolingCapacity":cooling_capacity,
+            "Compressor":compressor,
+            "RemoteControl":remote_control,
+            "DustFilter":dust_filter,
+            "PowerRequirement":power_requirement,
+            "PowerConsumption":power_consumption,
+            "Product_Url": product_url
+            
+        
+            
+        })
+        
+
+        data = pd.concat([data, pd.DataFrame.from_dict(df)], ignore_index=True)
+
+    # Add the data frame to the list of data frames
+    df_list.append(data)
+
+# Concatenate all the data frames in the list into a single data frame
+final_df = pd.concat(df_list, ignore_index=True)
+
+# Export the data frame as a CSV file
+final_df.to_csv("Airconditioner.csv", index=False)
+
+
+
---
+++ code/Washingmachine Scraping.py
@@ -0,0 +1,324 @@
+#Importing necessary libraries
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+#Function to extract content from page
+
+def url_content(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    return soup
+
+
+#Function for product url
+
+
+def get_product_url(soup):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    product_link= []
+    start_link = "https://www.flipkart.com"
+    for item in soup.find_all('a',href=True, attrs={'class':'_1fQZEK'}):
+        rest_link = item['href']
+        product_link.append(start_link+rest_link)
+    return product_link
+
+#Function to extract names of products 
+
+def get_product_name(soup):
+    product_name= []
+    try:
+        name=soup.find('span',attrs={'class':"B_NuCI"}).text
+        product_name.append(name)
+    except:
+        name = "No product name available"
+        product_name.append(name)
+    return product_name
+
+
+#Function to extract price of products 
+
+
+def get_product_price(soup):
+    product_price = []
+    try:
+        price = soup.find('div',attrs={'class':"_30jeq3 _16Jk6d"}).text
+        product_price.append(price)
+    except:
+        price = "NO price available"
+        product_price.append(price)
+    return product_price
+
+
+#Function to extract ratings of products 
+
+
+def get_product_ratings(soup):
+    product_ratings = []
+    try:
+        ratings = soup.find('div', attrs = {'class':"_3LWZlK"}).text
+        product_ratings.append(ratings)
+    except:
+        ratings = "No rating available"
+        product_ratings.append(ratings)
+    return product_ratings
+
+
+#Function to extract brand_name of products 
+
+
+def get_brand_name(soup):
+    brand_name = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "brand":
+                brand_name = cells[1].text.strip()
+                break
+        if not brand_name:
+            brand_name = "No value available"
+    except:
+         brand_name = "No value available"
+    return brand_name
+
+
+
+#Function to extract model_name of products
+
+def get_model_name(soup):
+    model_name = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "model name":
+                model_name = cells[1].text.strip()
+                break
+        if not model_name:
+            model_name = "No value available"
+    except:
+         model_name = "No value available"
+    return model_name
+
+
+#Function to extract washing_capacity of products
+
+
+def get_washing_capacity(soup):
+    washing_capacity = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "washing capacity":
+                washing_capacity = cells[1].text.strip()
+                break
+        if not washing_capacity:
+            washing_capacity = "No value available"
+    except:
+         washing_capacity = "No value available"
+    return washing_capacity
+
+
+#Function to extract color of products
+
+
+def get_color(soup):
+    color = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "color":
+                color = cells[1].text.strip()
+                break
+        if not color:
+            color = "No value available"
+    except:
+        color = "No value available"
+    return color
+
+
+#Function to extract fuction_type of products
+
+
+def get_function_type(soup):
+    function_type = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "function type":
+                function_type = cells[1].text.strip()
+                break
+        if not function_type:
+            function_type = "No value available"
+    except:
+         function_type = "No value available"
+    return function_type
+
+
+#Function to extract maxmimum spin speed of products
+
+
+def get_max_spin_speed(soup):
+    max_spin_speed = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "maximum spin speed":
+                max_spin_speed = cells[1].text.strip()
+                break
+        if not max_spin_speed:
+            max_spin_speed = "No value available"
+    except:
+        max_spin_speed = "No value available"
+    return max_spin_speed
+
+
+
+#Function to extract inbuilt_heater of products
+
+
+def get_inbuilt_heater(soup):
+    inbuilt_heater = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "in-built heater":
+                inbuilt_heater = cells[1].text.strip()
+                break
+        if not inbuilt_heater:
+            inbuilt_heater = "No value available"
+    except:
+         inbuilt_heater = "No value available"
+    return inbuilt_heater
+
+
+#Function to extract washing method of products
+
+
+def get_washing_method(soup):
+    washing_method = ""
+    try:
+        table = soup.find("div", {"class": "_3k-BhJ"})
+        rows = table.find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            if cells[0].text.strip().lower() == "washing method":
+                washing_method = cells[1].text.strip()
+                break
+        if not washing_method:
+            washing_method = "No value available"
+    except:
+         washing_method = "No value available"
+    return washing_method
+
+
+
+
+# Define the URL
+
+url_base = "https://www.flipkart.com/search?q=washing%20machine&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
+# Create an empty list to store data frames
+
+df_list = []
+
+# Scraping 
+
+for page in range(1,45):
+    # Define the URL for each page
+    url = url_base + str(page)
+    
+    # Get the content of the page
+    url_contents = url_content(url)
+    
+    # Get the URLs for each product on the page
+    product_links = get_product_url(url_contents)
+    
+    # Create an empty data frame
+    data = pd.DataFrame(columns=[
+        
+        
+        "Product Name",
+        "Price",
+        "Ratings",
+        "Brand Name",
+        "Model Name",
+        "Washing Capacity",
+        "Color",
+        "Maximum Spin Speed",
+        "Function Type",
+        "Inbuilt Heater",
+        "Washing Method",
+        "Product_Url"
+       
+    ])
+
+    # Loop through each product on the page
+    for product in range(len(product_links)):
+        # Get the content of the product page
+        product_url = product_links[product]
+        product_content = url_content(product_url)
+
+        # Get the product details and store them in a data frame
+        product_name = get_product_name(product_content)
+        product_price = get_product_price(product_content)
+        product_ratings = get_product_ratings(product_content)
+        brand_name = get_brand_name(product_content)
+        model_name = get_model_name(product_content)
+        washing_capacity = get_washing_capacity(product_content)
+        color = get_color(product_content)
+        max_spin_speed = get_max_spin_speed(product_content)
+        function_type = get_function_type(product_content)
+        inbuilt_heater = get_inbuilt_heater(product_content)
+        washing_method = get_washing_method(product_content)
+
+
+        df = pd.DataFrame({
+             "Product Name": product_name,
+             "Price": product_price,
+             "Ratings" : product_ratings,
+             "Brand Name": brand_name,
+             "Model Name": model_name,
+             "Washing Capacity": washing_capacity,
+             "Color": color,
+             "Maximum Spin Speed":max_spin_speed,
+             "Function Type":function_type,
+             "Inbuilt Heater": inbuilt_heater,
+             "Washing Method": washing_method,
+             "Product_Url": product_url
+
+
+             })
+        
+
+        data = pd.concat([data, pd.DataFrame.from_dict(df)], ignore_index=True)
+
+    # Add the data frame to the list of data frames
+    df_list.append(data)
+
+# Concatenate all the data frames in the list into a single data frame
+final_df = pd.concat(df_list, ignore_index=True)
+
+# Export the data frame as a CSV file
+final_df.to_csv("Washingmachine.csv", index=False)
+
+
+
+
+
+
